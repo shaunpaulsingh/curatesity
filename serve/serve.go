8e16db9f000a03f85aeb788bef7b4f37b628d024
Shaun Bharth <shaunpaulsingh@gmail.com>
2021-08-10 15:49:06 +0800

    Eh Siao

--- serve/serve.go
+++ serve/serve.go
@@ -1,34 +1,25 @@
 package serve
 
 import (
-	"flag"
-	"fmt"
 	"log"
 	"net/http"
+	"os"
 
-	"github.com/apex/gateway"
 	"github.com/gorilla/mux"
 )
 
 func Init() {
 	r := mux.NewRouter()
 
-	port := flag.Int("port", -1, "specify a port to use http rather than AWS Lambda")
-	flag.Parse()
-	listener := gateway.ListenAndServe
-	portStr := "n/a"
-	if *port != -1 {
-		portStr = fmt.Sprintf(":%d", *port)
-		listener = http.ListenAndServe
-		fs := http.FileServer(http.Dir("htdocs/css/"))
-		r.PathPrefix("/css/").Handler(http.StripPrefix("/css/", fs))
-		is := http.FileServer(http.Dir("htdocs/img/"))
-		r.PathPrefix("/img/").Handler(http.StripPrefix("/img/", is))
+	fs := http.FileServer(http.Dir("htdocs/css/"))
+	r.PathPrefix("/css/").Handler(http.StripPrefix("/css/", fs))
+	is := http.FileServer(http.Dir("htdocs/img/"))
+	r.PathPrefix("/img/").Handler(http.StripPrefix("/img/", is))
 
-		r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/", HomeHandler)
 
-		log.Fatal(listener(portStr, r))
-	}
+	port := os.Getenv("PORT")
+	log.Fatal(http.ListenAndServe(port, r))
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
