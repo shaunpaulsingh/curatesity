7dfe85da7e4b2d4ddcf2d0e42b8ace16287c5bb1
Shaun Bharth <shaunpaulsingh@gmail.com>
2021-08-10 15:38:07 +0800

    Boo

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module curatesity
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/apex/gateway v1.1.2
+	github.com/gorilla/mux v1.8.0
+)
--- serve/serve.go
+++ serve/serve.go
@@ -1,23 +1,34 @@
 package serve
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/apex/gateway"
 	"github.com/gorilla/mux"
 )
 
 func Init() {
 	r := mux.NewRouter()
 
-	fs := http.FileServer(http.Dir("htdocs/css/"))
-	r.PathPrefix("/css/").Handler(http.StripPrefix("/css/", fs))
-	is := http.FileServer(http.Dir("htdocs/img/"))
-	r.PathPrefix("/img/").Handler(http.StripPrefix("/img/", is))
+	port := flag.Int("port", -1, "specify a port to use http rather than AWS Lambda")
+	flag.Parse()
+	listener := gateway.ListenAndServe
+	portStr := "n/a"
+	if *port != -1 {
+		portStr = fmt.Sprintf(":%d", *port)
+		listener = http.ListenAndServe
+		fs := http.FileServer(http.Dir("htdocs/css/"))
+		r.PathPrefix("/css/").Handler(http.StripPrefix("/css/", fs))
+		is := http.FileServer(http.Dir("htdocs/img/"))
+		r.PathPrefix("/img/").Handler(http.StripPrefix("/img/", is))
 
-	r.HandleFunc("/", HomeHandler)
+		r.HandleFunc("/", HomeHandler)
 
-	log.Fatal(http.ListenAndServe(":8081", r))
+		log.Fatal(listener(portStr, r))
+	}
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
